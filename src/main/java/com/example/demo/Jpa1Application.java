364231cb12c7ddb8cf0a6f76c4f7eac1b7d9b22d
XXX <rnjsgurxo12345@gmail.com>
2024-12-04 15:28:17 +0900

    main update

--- src/main/java/com/example/demo/Jpa1Application.java
+++ src/main/java/com/example/demo/Jpa1Application.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 @SpringBootApplication
 public class Jpa1Application {
+	
+	private static final org.slf4j.Logger Logger = LoggerFactory.getLogger(Jpa1Application.class);
 
 	@Value("${file.uploadpath}")
 	String uploadPath;
@@ -20,6 +24,7 @@ public class Jpa1Application {
 	
 	@GetMapping("/")
 	public String index(Model model) {
+		Logger.info("main");
 		model.addAttribute("upload", uploadPath);
 		return "index.html";
 	}
